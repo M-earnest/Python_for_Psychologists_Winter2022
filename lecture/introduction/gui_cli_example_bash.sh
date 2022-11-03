745434bbf76e9ea2401dc1f0a3ffa14540a55d05
Michael Ernst <m.earnest211@gmail.com>
2022-11-03 10:47:59 +0100

    updated lecture notebooks for new course structure

--- lecture/introduction/gui_cli_example_bash.sh
+++ lecture/introduction/gui_cli_example_bash.sh
@@ -3,19 +3,19 @@ names=" pugsley gomez foster"
 
 for name in $names
 do
-    mkdir -p /Users/peerherholz/Desktop/adams_family/$name
-    cd /Users/peerherholz/Desktop/adams_family/$name
+    mkdir -p /home/michael/Desktop/adams_family/$name
+    cd /home/michael/Desktop/adams_family/$name
     for id in {1..10}
     do
         touch scooby_doo_$id.txt
     done
-    mkdir /Users/peerherholz/Desktop/the_new_yorker
+    mkdir /home/michaelDesktop/the_new_yorker
     for file in ./*
     do 
         file_id=$(echo "${file%.*}" | awk -F'[_.]' '{print $4}') 
         if [ $((file_id%2)) -eq 0 ];
         then
-        mv $file /Users/peerherholz/Desktop/the_new_yorker/${file//.txt/_$name.txt}
+        mv $file /home/michael/Desktop/the_new_yorker/${file//.txt/_$name.txt}
         fi
     done
 done
---
+++ lecture/introduction/hello_statement.py
@@ -0,0 +1,19 @@
+print("Hello world!")
+
+def some_func():
+    """
+    a simple greeting
+    """
+    print("How'r you now?")
+
+def some_other_func():
+    """
+    a simple response to a greeting
+    """
+    print("Not'so bad 'n'you?")
+
+def some_other_other_func():
+    """
+    a different response to a greeting
+    """
+    print("Good 'n'you?")
